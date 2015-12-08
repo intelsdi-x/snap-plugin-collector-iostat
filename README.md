cd5864e07a8661866fb4231184aeb262e0d75e4f
Joel Cooklin <joel.cooklin@gmail.com>
2015-12-08 10:58:12 -0800

    Adds plugin option for getting results since boot

--- README.md
+++ README.md
@@ -30,6 +30,7 @@ In order to use this plugin you need "iostat" to be installed on a Linux target 
 
 * Linux OS
 * [sysstat package] (#installation)
+ * sysstat 10.2 or greater
 * [golang 1.4+](https://golang.org/dl/)
 
 The iostat command-line tool is part of the sysstat package available under the GNU General Public License.
--- iostat/iostat.go
+++ iostat/iostat.go
@@ -29,6 +29,7 @@ import (
 	"github.com/intelsdi-x/snap-plugin-collector-iostat/iostat/parser"
 	"github.com/intelsdi-x/snap/control/plugin"
 	"github.com/intelsdi-x/snap/control/plugin/cpolicy"
+	"github.com/intelsdi-x/snap/core/ctypes"
 )
 
 const (
@@ -65,7 +66,7 @@ func New() (*IOSTAT, error) {
 
 // CollectMetrics returns metrics from iostat
 func (iostat *IOSTAT) CollectMetrics(mts []plugin.PluginMetricType) ([]plugin.PluginMetricType, error) {
-	_, data, err := iostat.run()
+	_, data, err := iostat.run(mts)
 	if err != nil {
 		return nil, err
 	}
@@ -86,7 +87,7 @@ func (iostat *IOSTAT) CollectMetrics(mts []plugin.PluginMetricType) ([]plugin.Pl
 
 // GetMetricTypes returns the metric types exposed by iostat
 func (iostat *IOSTAT) GetMetricTypes(_ plugin.PluginConfigType) ([]plugin.PluginMetricType, error) {
-	keys, _, err := iostat.run()
+	keys, _, err := iostat.run([]plugin.PluginMetricType{})
 	if err != nil {
 		return nil, err
 	}
@@ -104,7 +105,27 @@ func (iostat *IOSTAT) GetConfigPolicy() (*cpolicy.ConfigPolicy, error) {
 }
 
 // Init initializes iostat plugin
-func (iostat *IOSTAT) run() ([]string, map[string]float64, error) {
+func (iostat *IOSTAT) run(mts []plugin.PluginMetricType) ([]string, map[string]float64, error) {
+	// TODO: reminder - remove these todo statements from the README (roadmap section) once
+	//       they are completed
+	// TODO: add validation that we are running sysstat version 10.2.0 or greater else print a
+	//       a warning or otherwise communicate to the user that they are potentially using an
+	//       unsupported/tested configuration
+	// TODO: allow the path and/or name of the command to be overriden through the pluginConfigType
+
+	reader, err := iostat.cmd.Run("iostat", getArgs(mts))
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return iostat.parser.Parse(reader)
+}
+
+// updateArgs will add -y to the args provided to iostat telling iostat to report stats
+// since the machine has booted if the config ReportSinceBoot is present and True.  The
+// config for each metric being requested is the same so we need to check the config for
+// one metric being requested.
+func getArgs(mts []plugin.PluginMetricType) []string {
 	/////////////////////////////////////////////////////////////////////////////////////////
 	// 	IOstat command with interval 1 and options:
 	// 		-c	 	display the CPU utilization report
@@ -114,23 +135,29 @@ func (iostat *IOSTAT) run() ([]string, map[string]float64, error) {
 	// 		-x		display extended statistics
 	// 		-k		display bandwidth statistics in kilobytes per second
 	// 		-t		print the time for each report displayed
+	//      -y      excludes report since last report (not since boot)
 	////////////////////////////////////////////////////////////////////////////////////////
+	iostatArgs := []string{"-c", "-d", "-p", "-g", "ALL", "-x", "-k", "-t"}
 
-	// TODO: reminder - remove these todo statements from the README (roadmap section) once
-	//       they are completed
-	// TODO: add validation that we are running sysstat version 10.2.0 or greater else print a
-	//       a warning or otherwise communicate to the user that they are potentially using an
-	//       unsupported/tested configuration
-	// TODO: allow the path and/or name of the command to be overriden through the pluginConfigType
-
-	args := []string{"-c", "-d", "-p", "-g", "ALL", "-x", "-k"}
-
-	reader, err := iostat.cmd.Run("iostat", args)
-	if err != nil {
-		return nil, nil, err
+	reportLatest := true
+	if len(mts) > 0 && mts[0].Config() != nil && len(mts[0].Config().Table()) > 0 {
+		if m, ok := mts[0].Config().Table()["ReportSinceBoot"]; ok {
+			switch val := m.(type) {
+			case ctypes.ConfigValueBool:
+				if val.Value {
+					// return without adding '-y' to the args
+					// produces results since boot
+					reportLatest = false
+				}
+			}
+		}
 	}
-
-	return iostat.parser.Parse(reader)
+	if reportLatest {
+		// -y will disregards the summary since boot
+		// the "1" "1" arguments will produce 1 result over a 1 second interval
+		iostatArgs = append(iostatArgs, "-y", "1", "1")
+	}
+	return iostatArgs
 }
 
 func joinNamespace(ns []string) string {
